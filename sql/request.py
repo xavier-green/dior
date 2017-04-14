34de7ea4ea66e4d03541648e6c66745bc9a7fdd6
LE ROUX--COULOIGNER Solen <solen.leroux.c@gmail.com>
2017-04-14 12:50:56 +0200

    added tables

--- sql/request.py
+++ sql/request.py
@@ -4,12 +4,23 @@ from tables import item
 
 class query(object):
     def __init__ (self, table, columns):
-        self.columns = columns
-        self.table = table
-        self.request = "SELECT TOP 3 " + self.columns + " FROM " + self.table.name + " AS " + table.alias + "\n"
-        self.joints = [self.table]
+        Bool = True
+        for c in columns :
+            if c not in table.columns:
+                Bool = False
+        assert Bool or columns[0] == '*', "La table " + table.name + " ne possède pas un des attributs demandés parmi " + ', '.join(columns)
+
+        if columns[0] != "*" :
+            objectif = table.alias + '.' + table.prefix + columns[0]
+            for i in range(1, len(columns)):
+                objectif += ', ' + table.alias + '.' + table.prefix + columns[i]
+        else :
+            objectif = "*"
         
-    def add_joint(self, table1, table2, join1, join2):
+        self.request = "SELECT TOP 3 " + objectif + " FROM " + table.name + " AS " + table.alias + "\n"
+        self.joints = [table]
+        
+    def join(self, table1, table2, join1, join2):
         assert table1 in self.joints, "Vous tentez de joindre deux tables absentes de la requête."
         assert join1 in table1.columns, "La table " + table1.name + " ne contient pas d'attribut " + table1.prefix + join1
         assert join2 in table2.columns, "La table " + table2.name + " ne contient pas d'attribut " + table2.prefix + join2
@@ -19,15 +30,14 @@ class query(object):
         jointure2 = table2.alias + "." + table2.prefix + join2
         self.request += "JOIN " + table2.name + " AS " + table2.alias + " ON "  + jointure1 + " = " + jointure2 + "\n"
         
-    def add_country(self, country):
-        # A modifier suivant la table
-        self.request += "WHERE " + self.table.prefix + "country LIKE '%" + country + "%'\n"
+    def where(self, table, column, description):
+        assert table in self.joints, "Vous faites appel à la table " + table.name + " absente de la requête, utilisez JOIN pour l'ajouter"
+        assert column in table.columns, "La table " + table.name + " ne possède pas d'attribut " + table.prefix + column
         
-    def add_description(self, description):
-        if "Description" in self.table.columns :
-            self.request += "WHERE " + self.table.alias + '.' + self.table.prefix + "Description LIKE '%" + description + "%'\n"
-        else:
-            print("Cette table ne possède pas d'attribut 'Description'")
+        self.request += "WHERE " + table.alias + '.' + table.prefix + column + " LIKE '%" + description + "%'\n"
+    
+    def groupby(self, table, column):
+        assert table in self.joints,  "Vous faites appel à la table " + table.name + " absente de la requête, utilisez JOIN pour l'ajouter"
         
     def write_query(self):
         self.request += "GO\n"
@@ -36,8 +46,14 @@ class query(object):
         self.file.write(self.request)
         self.file.close()
 
-test = query(item, '*')
-test.add_joint(item, color_size, "Code", "Code")
-test.add_description('bustier')
+test = query(item, ['*'])
+test.join(item, color_size, "Code", "Code")
+test.where(item, 'Description', 'bustier')
 test.write_query()
-print(test.request)
+
+test20 = query(color_size, ['Color'])
+test20.join(color_size, item, 'Code', 'Code')
+test20.where(item, 'Description', 'rose des vents')
+test20.write_query()
+
+#print(test20.request)
--- sql/tables.py
+++ sql/tables.py
@@ -8,5 +8,30 @@ class table(object):
         self.columns = columns
 
 country = table("COUN_COUNTRY", "COUN_", "CO", ["Description", "Zone", "Currency", "Sub_Zone", "Description_FR"])
-item = table("ITEM_ITEM", "ITEM_", "IT", ["Code", "Description"])
+nationality = table("CRM_Country_Nationality", "CRM_", "CN", ["Code_ISO", "Country_Desc_FR", "Country_Desc_EN", "Nationality_Desc_FR", "Nationality_Desc_EN", "Zone", "Sub_Zone"])
+customer = table("CUST_CUSTOMER", "CUST_", "CU", [""])
+department = table("DEPT_Department", "DEPT_", "DE", [""])
+division = table("DIVI_Division", "DIVI_", "DI", [""])
+family = table("FAMI_family", "FAMI_", "FA", [""])
+group = table("grou_group", "GROU_", "GR", [""])
 color_size = table("ITEM_COLOR_SIZE", "ITEM_", "CS", ["Code", "Color", "Size"])
+item = table("ITEM_ITEM", "ITEM_", "IT", ["Code", "Description", "Year", "Collection", "Division", "Line", "Family", "Sub_Family", "Theme", "Department", "Group", "Vendor", "Series", "RS_Style", "Segment", "Material", "Price_Cat", "Model"])
+boutique = table("loca_location", "LOCA_", "BT", [""])
+model = table("mod_model", "MOD_", "MD", [""])
+sale = table("sale_sales", "SALE_", "SA", [""])
+sub_family = table("SFAM_sub_Family", "SFAM_", "SF", [""])
+staff = table("STAFF_staff", "STAFF_", "STF", [""])
+state = table("STAT_STATE", "STAT_", "STT", [""])
+stock_daily = table("stoc_stocks_Daily", "", "", [""])
+stock_weekly = table("stoc_stocks_week", "", "", [""])
+sub_zone = table("SZONE_Sub_Zone", "SZONE_", "SZ", [""])
+theme = table("THEM_Theme", "THEM_", "TH", [""])
+uzone = table("UZONE_Zone", "UZONE_", "UZ", [""])
+zone = table("ZONE_Zone", "ZONE_", "ZO", [""])
+
+
+
+#sale_sales_Random
+#stoc_stocks_Daily
+#stoc_stocks_week
+#sysdiagrams
