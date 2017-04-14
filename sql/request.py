46747330181b34b0a10f6372ac90021a64dfce47
Rémi Garde <remi.garde@free.fr>
2017-04-14 17:02:07 +0200

    Fix: request.write() now exludes the pointless last line

--- sql/request.py
+++ sql/request.py
@@ -57,5 +57,8 @@ class query(object):
 
     def write(self):
         self.request += "GO\n"
+        # Database call
         p = subprocess.run('docker exec  mssql /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P D10R_password! -d Reporting -W -w 999 -s | -Q'.split() + [self.request], stdout=subprocess.PIPE, universal_newlines=True)
+        # Delete last line '(n rows affected)'
+        out = p.stdout.splitlines()[:-1]
         return(csv.DictReader(p.stdout.splitlines(), delimiter='|'))
