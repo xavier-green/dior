6f4e5f63f2abe684aa7df35dcd2b26201745aa80
xavier-green <xavier.green@student.ecp.fr>
2017-04-18 11:22:26 +0200

    adding tfidf in model

--- intent/intent.py
+++ intent/intent.py
@@ -18,6 +18,7 @@ not_stop_words = ["ou","où","qui","quand","quel","quelle","quelle"]
 stop_words = [x for x in stop_words_old if x not in not_stop_words]
 from sklearn import neighbors
 from sklearn.externals import joblib
+from sklearn.feature_extraction.text import TfidfTransformer
 
 def tokenize(text):
     stripped_punctuation = re.sub(r'[-_;,.?!]',' ',text.lower())
@@ -71,6 +72,7 @@ class intentModel(object):
         print("Done setting up X and y")
         word2vec = Pipeline([
             ("word2vec vectorizer", Word2VecVectorizer(self.word2vec_model)),
+            ('tfidf', TfidfTransformer(use_idf=False)),
             ("knn neighbours", neighbors.KNeighborsClassifier(15, weights='distance'))])
         word2vec.fit(X,y)
         self.trained_model = word2vec
