70e7966896f7c0aa3746bce1279a0904463be8b4
Rémi Garde <remi.garde@free.fr>
2017-04-19 15:35:30 +0200

    No need for try...except

--- response.py
+++ response.py
@@ -67,9 +67,8 @@ JOIN (
 )
 AS SA ON STF.STAFF_Code = SA.SALE_Staff
 GROUP BY STF.STAFF_Name ORDER BY sum(SA.SALE_Std_RP_WOTax_REF)/count(*) desc"""
-		try:
-			sock.connect('./tmp/request.sock')
-			sock.sendall(bytes(request, 'utf-8'))
-			out = sock.recv(8192).decode('utf-8').splitlines()[:-2]
-			out.pop(1)
-		return resp + "\n".join(out)
+		sock.connect('./tmp/request.sock')
+		sock.sendall(bytes(request, 'utf-8'))
+		out = sock.recv(8192).decode('utf-8').splitlines()[:-2]
+		out.pop(1)
+		return(resp + "\n".join(out))
