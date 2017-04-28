c111dddca4cd7900f9cc4884b543e108a326abf2
Rémi Garde <remi.garde@free.fr>
2017-04-28 16:31:25 +0200

    Print the whole list of countries/cities/boutiques

--- intent/stock.py
+++ intent/stock.py
@@ -138,20 +138,20 @@ class Stock(object):
 			sellthru = format((100 * res_sales / (res_sales  + res_stock)), '.2f') + ' %'
 			res_sellthru = 'Le sellthru '
 			if len(self.boutiques) > 0:
-				res_sellthru += "dans la boutique " + self.boutiques + ' '
+				res_sellthru += "dans la boutique " + ' '.join(self.boutiques) + ' '
 			if len(self.cities) > 0:
-				res_sellthru += "dans la ville"  + self.cities + ' '
+				res_sellthru += "dans la ville"  + ' '.join(self.cities) + ' '
 			if len(self.countries) > 0:
-				res_sellthru += 'dans le pays ' + self.countries + ' '
+				res_sellthru += 'dans le pays ' + ' '.join(self.countries) + ' '
 			res_sellthru += "est de " + sellthru
 			return [stock_query.request + '\n' + product_query.request,res_sellthru ]
 			str_res_stock = 'Le stock '
 			if len(self.boutiques) > 0:
-				str_res_stock += "dans la boutique " + self.boutiques + ' '
+				str_res_stock += "dans la boutique " + ' '.join(self.boutiques) + ' '
 			if len(self.cities) > 0:
-				str_res_stock += "dans la ville"  + self.cities + ' '
+				str_res_stock += "dans la ville"  + ' '.join(self.cities) + ' '
 			if len(self.countries) > 0:
-				str_res_stock += 'dans le pays ' + self.countries + ' '
+				str_res_stock += 'dans le pays ' + ' '.join(self.countries) + ' '
 			res_sellthru += "est de " + str(res_stock)
 		return(stock_query.request, str_res_stock)
 
