d23523bc4480ed0d1407cc14ed4f24036043d96a
LE ROUX--COULOIGNER Solen <solen.leroux.c@gmail.com>
2017-05-12 14:18:16 +0200

    new fonction annexe

---
+++ intent/fonctions_annexes.py
@@ -0,0 +1,10 @@
+def what_products(liste_of_dict):
+	assert type(list_of_dict) is list, "Expected a list"
+	assert type(list_of_dict[0]) is dict, "Expected the list to contain dict"
+
+	
+
+
+"""
+Xavier écris en dessous
+"""
