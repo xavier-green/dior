ff884d39701761b300bf51d9235342e701ea6cff
xavier-green <xavier.green@student.ecp.fr>
2017-05-16 15:49:05 +0200

    sources added to products

--- extraction/product.py
+++ extraction/product.py
@@ -41,39 +41,48 @@ class ProductExtractor(object):
         self.order = [
             {"division": {
                 "file": self.division,
-                "column": self.division.Division
+                "column": self.division.Division,
+                "single": 'Division'
             }},
             {"departement": {
                 "file": self.departement,
-                "column": self.departement.Departement
+                "column": self.departement.Departement,
+                "single": 'Departement'
             }},
             {"famille": {
                 "file": self.famille,
-                "column": self.famille.Famille
+                "column": self.famille.Famille,
+                "single": 'Famille'
             }},
             {"color": {
                 "file": self.color,
-                "column": self.color.Color
+                "column": self.color.Color,
+                "single": 'Color'
             }},
             {"groupe": {
                 "file": self.groupe,
-                "column": self.groupe.Groupe
+                "column": self.groupe.Groupe,
+                "single": 'Groupe'
             }},
             {"material": {
                 "file": self.material,
-                "column": self.material.Material
+                "column": self.material.Material,
+                "single": 'Material'
             }},
             {"shape": {
                 "file": self.shape,
-                "column": self.shape.Shape
+                "column": self.shape.Shape,
+                "single": 'Shape'
             }},
             {"theme": {
                 "file": self.theme,
-                "column": self.theme.Theme
+                "column": self.theme.Theme,
+                "single": 'Theme'
             }},
             {"produit": {
                 "file": self.produit,
-                "column": self.produit.Produit
+                "column": self.produit.Produit,
+                "single": 'Produit'
             }}
         ]
         print("Cleaning csv ...")
@@ -133,11 +142,13 @@ class ProductExtractor(object):
         # self.clean_theme_csv()
 
     def csv_contains(self, w, csv_file, csv_column):
-        #print(self.csv[self.csv.Produit.str.contains(" "+w.rstrip().lstrip()+" ")])
         return csv_file[csv_column.str.contains(" "+w.rstrip().lstrip()+" ")]
 
-    def get_product(self, sentence, csv_file, csv_column):
-        return len(self.csv_contains(sentence, csv_file, csv_column))>0
+    def get_product(self, sentence, csv_file, csv_column, csv_single):
+        csv_matches = self.csv_contains(sentence, csv_file, csv_column, csv_single)
+        if len(csv_matches)>0:
+            return list(csv_matches[single_column])
+        return None
 
     def aggressive_tokenize(self, text):
         min_length = 3
@@ -156,6 +167,7 @@ class ProductExtractor(object):
 
     def extract(self, sentence):
         results = []
+        sources = []
         pattern = re.compile('^[a-zA-Z0-9 ]')
         sentence = sentence.lower()
         sentence = re.sub(r'[^\w\s]','',sentence)
@@ -168,14 +180,17 @@ class ProductExtractor(object):
             for orde in self.order:
                 for key in orde:
                     # print("extraction en cours pour "+key)
-                    results, sentence = self.extract_N(sentence, tags_dict, orde[key], key, results, i)
+                    results, sentence, sources = self.extract_N(sentence, tags_dict, orde[key], key, results, sources, i)
+        print("$$$$$$$$$$$$$ Sources")
+        print(sources)
         return results
 
-    def extract_N(self, sentence, tags_dict, csv, bdd, prev_results, n):
+    def extract_N(self, sentence, tags_dict, csv, bdd, prev_results, prev_sources, n):
         prev_results_copy = prev_results[:]
         sentence_tokens = sentence.split(" ")
         ng = ngrams(sentence_tokens,n)
         ok_products = []
+        sources = []
         for item in ng:
             short_sentence = " ".join(item)
             auth = not (len([w for w in self.not_replace if w in short_sentence])>0 or len([w for w in item if w not in tags_dict])>0)
@@ -186,11 +201,13 @@ class ProductExtractor(object):
                 if words_tags[:-1] in self.authorized:
                     #print(short_sentence)
                     #print("getting product for "+str(csv))
-                    if self.get_product(short_sentence, csv["file"], csv["column"]):
+                    products_matched = self.get_product(short_sentence, csv["file"], csv["column"], csv["single"])
+                    if products_matched:
                         # print("ok")
                         matched_item = {}
                         matched_item[bdd] = short_sentence
                         ok_products.append(matched_item)
+                        sources += products_matched
                         for i in range(len(prev_results_copy)):
                             for key in prev_results_copy[i]:
                                 if prev_results_copy[i][key] in short_sentence:
@@ -198,7 +215,7 @@ class ProductExtractor(object):
         for el in ok_products:
             for key in el:
                 sentence = sentence.replace(el[key],'')
-        return (ok_products+[a for a in prev_results_copy if a != ''],sentence)
+        return (ok_products+[a for a in prev_results_copy if a != ''],sentence, sources)
 
     def clean_text(self, text):
         copy = text[:].lower()
