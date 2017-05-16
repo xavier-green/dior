e31965c0c93348168c7121d5ddb18c33ad2d69cc
xavier-green <xavier.green@student.ecp.fr>
2017-05-16 14:44:07 +0200

    date fixed

--- annexes/gestion_details.py
+++ annexes/gestion_details.py
@@ -2,7 +2,7 @@ from annexes.mise_en_forme import affichage_euros, affichage_date
 from annexes.gestion_dates import today, last_monday
 from annexes.gestion_products import what_products
 
-from datetime import datetime
+from datetime
 
 def find_category(categorie):
 	resp = categorie[0:4]
@@ -25,15 +25,16 @@ def find_category(categorie):
 def append_details_date(details, numerical_dates):
 	start_date = numerical_dates[0][0] if len(numerical_dates) > 0 else last_monday()
 	end_date = numerical_dates[0][1] if len(numerical_dates) > 0 else today()
+	end_date = end_date+datetime.timedelta(days=-1)
 	dateFormat = "%Y%m%d"
-	a = datetime.strptime(start_date, dateFormat)
-	b = datetime.strptime(end_date, dateFormat)
+	a = datetime.datetime.strptime(start_date, dateFormat)
+	b = datetime.datetime.strptime(end_date, dateFormat)
 	delta = b - a
 	days_diff = delta.days
 	print("Days difference: "+str(days_diff))
 	if days_diff>1:
 		details.append(["Du", affichage_date(start_date)])
-		details.append(["Au (non inclu)", affichage_date(end_date)])
+		details.append(["Au", affichage_date(end_date)])
 	else:
 		details.append(["Le", affichage_date(start_date)])
 	return details
