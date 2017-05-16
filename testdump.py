69dc2f4edcd2ba3d9f3305c52fb5e8ea821c1218
Remi GARDE <rgarde_ext@christiandior.com>
2017-05-16 12:48:55 +0200

    Departments in french...

--- testdump.py
+++ testdump.py
@@ -4,7 +4,7 @@ cmd1='sqlcmd -l 10 -S 10.148.102.166\DEV2012 -U REP_SQL_CHATBOT -P ChatBoT1984! 
 cmd2='_description) from '
 
 tables=[ 'LOCA_location', 'COLO_color', 'COUN_country', 'DEPT_department', 'DIVI_division' , 'FAMI_family', 'GROU_group', 'MATE_material', 'ITEM_item', 'SHAP_shape', 'STAT_state', 'SZONE_sub_zone', 'THEM_theme', 'UZONE_zone', 'ZONE_zone' ]
-files=['Boutiques', 'color', 'country', 'Departments', 'Divisions', 'family', 'Groupe', 'material', 'products', 'shape', 'state', 'szone', 'Themes', 'uzone', 'zone']
+files=['Boutiques', 'color', 'country', 'Departements', 'Divisions', 'family', 'Groupe', 'material', 'products', 'shape', 'state', 'szone', 'Themes', 'uzone', 'zone']
 
 for index, x in enumerate(tables):
     m = x[:x.find('_')]
