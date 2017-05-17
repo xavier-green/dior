efd0072d08bf96869b746bf833c8cbe182661a64
Remi GARDE <rgarde_ext@christiandior.com>
2017-05-17 15:16:48 +0200

    Comment the BATCH files

--- dumps_desc.bat
+++ dumps_desc.bat
@@ -1,3 +1,6 @@
+REM Ce fichier doit être inscrit dans le Task Scheduler de Windows / Crontab de *NIX. 
+REM Lancé 1-3 fois par semaines, il permet de mettre à jour les csv de data/ 
+REM qui contiennent les noms des produits / familles / ...
 chcp 65001
 d:/Anaconda/python.exe d:/dior/testdump.py
 pause
--- launch.bat
+++ launch.bat
@@ -1,3 +1,5 @@
+REM Ce fichier doit être inscrit dans le Task Scheduler de Windows / Crontab de *NIX. 
+REM Le configurer pour être lancé à chaque démarrage de la machine
 @echo off
 chcp 65001
 CD /D D:/dior
