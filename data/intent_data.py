ff619b97110d554688f61e9607c7bcb5a02855ae
Remi GARDE <rgarde_ext@christiandior.com>
2017-05-18 16:41:09 +0200

    Intentions for sellthru

--- data/intent_data.py
+++ data/intent_data.py
@@ -37,6 +37,10 @@ class intentData():
             ]
         },"stock": {
             "examples" : [
+		"Sellthru de la Cruise",
+		"Quel est le sellthru de la Spring en Asie",
+		"Quel est le sellthru de la fall à Montaigne",
+		"Sellthru de la Winter en bags au japon",
                 "Couverture de socks de bags ",
                 "quel est la couverture de stock de rose des vents sur 6 mois",
                 "Quel est la couverture au Japon sur trois mois",
