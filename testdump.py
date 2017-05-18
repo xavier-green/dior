4bf913075eedfe516e3e0773a57a7dcdc01c2473
Remi GARDE <rgarde_ext@christiandior.com>
2017-05-18 16:03:14 +0200

    Dumps Collection table

--- testdump.py
+++ testdump.py
@@ -3,8 +3,8 @@ import subprocess
 cmd1='sqlcmd -l 10 -S 10.148.102.166\DEV2012 -U REP_SQL_CHATBOT -P ChatBoT1984! -d Reporting_CDS -W -w 999 -f 65001 -s # -Q "SELECT distinct('
 cmd2='_description) from '
 
-tables=[ 'LOCA_location', 'COLO_color', 'COUN_country', 'DEPT_department', 'DIVI_division' , 'FAMI_family', 'GROU_group', 'MATE_material', 'ITEM_item', 'SHAP_shape', 'STAT_state', 'SZONE_sub_zone', 'THEM_theme', 'UZONE_zone', 'ZONE_zone' ]
-files=['Boutiques', 'color', 'country', 'Departements', 'Divisions', 'family', 'Groupe', 'material', 'products', 'shape', 'state', 'szone', 'Themes', 'uzone', 'zone']
+tables=[ 'LOCA_location', 'COLO_color', 'COUN_country', 'DEPT_department', 'DIVI_division' , 'FAMI_family', 'GROU_group', 'MATE_material', 'ITEM_item', 'SHAP_shape', 'STAT_state', 'SZONE_sub_zone', 'THEM_theme', 'UZONE_zone', 'ZONE_zone', 'COLL_Collection' ]
+files=['Boutiques', 'color', 'country', 'Departements', 'Divisions', 'family', 'Groupe', 'material', 'products', 'shape', 'state', 'szone', 'Themes', 'uzone', 'zone', 'collection']
 
 for index, x in enumerate(tables):
     m = x[:x.find('_')]
@@ -14,7 +14,8 @@ for index, x in enumerate(tables):
 
     if x == 'COUN_country' or x == 'UZONE_zone':
         tot = cmd1 + m + '_Description_FR) from '+x+'"'
-
+    if x == 'COLL_Collection':
+        tot = tot[:-1] + " where COLL_Description not like '%collection%'" + '"'
     with open('d:/dior/data/'+files[index]+'.csv', 'w', encoding='utf-8') as f:
             p = subprocess.Popen(tot, shell=True, encoding='utf-8', stdout=subprocess.PIPE)
             f.write("".join(p.stdout.readlines()[2:-1]))
