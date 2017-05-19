5483b52e053af4331c619ceb1e454de36749db6a
LE ROUX--COULOIGNER Solen <solen.leroux.c@gmail.com>
2017-05-19 11:34:17 +0200

    message vendeur

--- intent/vendeur.py
+++ intent/vendeur.py
@@ -92,6 +92,8 @@ class Vendeur(object):
 		reponse = "Voici les meilleurs vendeurs :"
 
 		liste_resultat = result.split("\n")
+		if len(ligne_resultat) < 2:
+			reponse = "\nAucun vendeur n'a réalisé ce genre de vente durant cette période."
 		for n, ligne in enumerate(liste_resultat):
 			if n == 0:
 				pass
