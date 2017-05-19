1992c8572f5b15d8250a3cfff90d7b7495679f14
LE ROUX--COULOIGNER Solen <solen.leroux.c@gmail.com>
2017-05-19 10:54:20 +0200

    verbose

--- sql/request.py
+++ sql/request.py
@@ -43,7 +43,7 @@ class query(object):
 						method_columns.append(method_table.alias + '.' + method_table.prefix + method_column)
 					response.append(' + '.join(method_columns))
 			else:
-				assert False, "Il semblerait que les colonnes demandées n'appartiennent à aucune table"
+				assert False, "Impossible de proprifier les colonnes suivantes :", c
 		# Pour les requêtes comme ORDER BY et GROUP BY qui demandent uniquement une colonne
 		if max_columns > 0:
 			response = response[0:max_columns]
